ff2e0f38bf91bd025d24ef7f56ce6df66a8b1f11
MikeWang <it512@msn.com>
2019-02-22 12:52:07 +0800

    add Listener

--- serve.go
+++ serve.go
@@ -2,7 +2,9 @@ package twig
 
 import (
 	"context"
+	"net"
 	"net/http"
+	"time"
 )
 
 // Server Http处理器接口
@@ -44,5 +46,56 @@ func (w *Work) Start() (err error) {
 
 // -------------------------------------- New
 
-type Connector interface {
+func MustListener(l net.Listener, err error) net.Listener {
+	if err != nil {
+		panic(err)
+	}
+	return l
+}
+
+type KeepAliveListener struct {
+	*net.TCPListener
+	count uint64
+}
+
+func NewKeepAliveListener(address string) (l *KeepAliveListener, err error) {
+	var addr *net.TCPAddr
+	var ln *net.TCPListener
+
+	if addr, err = net.ResolveTCPAddr("tcp", address); err != nil {
+		return
+	}
+
+	if ln, err = net.ListenTCP("tcp", addr); err != nil {
+		return
+	}
+
+	l = &KeepAliveListener{TCPListener: ln}
+
+	return
+}
+
+func (l *KeepAliveListener) Accept() (net.Conn, error) {
+	conn, err := l.AcceptTCP()
+	if err != nil {
+		return nil, err
+	}
+	conn.SetKeepAlive(true)
+	conn.SetKeepAlivePeriod(3 * time.Minute)
+	l.count++
+
+	return &tcpConn{
+		KeepAliveListener: l,
+		Conn:              conn,
+	}, nil
+}
+
+type tcpConn struct {
+	net.Conn
+	*KeepAliveListener
+}
+
+func (c tcpConn) Close() error {
+	c.KeepAliveListener.count--
+	return c.Conn.Close()
 }
